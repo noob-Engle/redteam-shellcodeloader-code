ca4361f6e75675b4a72c0e4c5569c1f984d28a9a
Aloneness <82130997+noob-Engle@users.noreply.github.com>
2022-03-24 17:49:29 +0800

    Add files via upload

---
+++ Generating pe/implant.cpp
@@ -0,0 +1,19 @@
+/*
+
+ Red Team Operator course code template - EXE
+ author: reenz0h (twitter: @sektor7net)
+
+*/
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+
+	printf("RT Operator, here I come!\n");
+	
+	getchar(); //getchar()函数以字符为单位对输入的数据进行读取
+    
+	return 0; //"return 0"的作用，就是返回了一個标识，表示正常退出。
+}
